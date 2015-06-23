49570a3ca1f47dc53d87c3295ae7fc2062813e30
madvas <matus.lestan@gmail.com>
2015-06-23 12:12:11 +0200

    "[GIT AUTO COMMIT]: Tue Jun 23 2015 12:12:11 GMT+0200 (CEST)"

--- Assets/Scripts/Managers/PositionManager.cs
+++ Assets/Scripts/Managers/PositionManager.cs
@@ -7,10 +7,12 @@ public static class PositionManager
 
 	public static PositionData GetRandomSpawnPosition ()
 	{
-		float offset = 3;
+		float offset = 0;
 		Debug.Log (floorBounds);
 		float randomX = Random.Range (floorBounds.center.x + (floorBounds.extents.x / 2) - offset, floorBounds.center.x - (floorBounds.extents.x / 2) + offset);
 		float randomZ = Random.Range (floorBounds.center.z + (floorBounds.extents.z / 2) - offset, floorBounds.center.z - (floorBounds.extents.z / 2) + offset);
+		Debug.Log (randomX);
+		Debug.Log (randomZ);
 		Vector3 randomPosition = new Vector3 (randomX, 0, randomZ);
 		randomPosition = ValidateSpawnPosition (randomPosition);
 		return new PositionData (randomPosition, Quaternion.identity);
