c88df7dd835e53b9fe473e5b449b1eb7cbb014dc
madvas <matus.lestan@gmail.com>
2015-06-23 19:24:15 +0200

    "[GIT AUTO COMMIT]: Tue Jun 23 2015 19:24:15 GMT+0200 (CEST)"

--- Assets/Scripts/UI/ScoreboardCanvas.cs
+++ Assets/Scripts/UI/ScoreboardCanvas.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class ScoreboardCanvas : MonoBehaviour
 {
 	Canvas canvas;
-	
+
+	void Awake ()
+	{
+		canvas = GetComponent<Canvas> ();
+	}
+
 	void Start ()
 	{
-		Debug.Log ("Scoreboard Canvas");
-		canvas = GetComponent<Canvas> ();
+		NetworkManager.onPlayerPropertiesChanged += OnPlayerPropertiesChanged;
 	}
 	
 	void Update ()
@@ -16,5 +20,12 @@ public class ScoreboardCanvas : MonoBehaviour
 		if (Input.GetKeyDown (KeyCode.Tab)) {
 			canvas.enabled = !canvas.enabled;
 		}
+	}
+
+	void OnPlayerPropertiesChanged (PhotonPlayer player, Hashtable props)
+	{
+		Debug.Log ("Player properties changed");
+		Debug.Log (player);
+		Debug.Log (props);
 	}
 }
