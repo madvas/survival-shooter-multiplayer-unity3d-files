862e6a9321522ccc516c2e88a6fb7f60f04059ea
madvas <matus.lestan@gmail.com>
2015-06-24 11:53:34 +0200

    "[GIT AUTO COMMIT]: Wed Jun 24 2015 11:53:34 GMT+0200 (CEST)"

--- Assets/Scripts/Helpers/ComponentHelper.cs
+++ Assets/Scripts/Helpers/ComponentHelper.cs
@@ -1,9 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class ComponentHelper : MonoBehaviour {
+public class ComponentHelper : MonoBehaviour
+{
 
-	public static sendMessageToAllComponents() {
+	public static void sendMonoMessage (string methodName, params object[] parameters)
+	{
+		HashSet<GameObject> objectsToCall;
+		objectsToCall = PhotonNetwork.FindGameObjectsWithComponent (typeof(MonoBehaviour));
+		
+		object callParameter = (parameters != null && parameters.Length == 1) ? parameters [0] : parameters;
+		foreach (GameObject gameObject in objectsToCall) {
+			gameObject.SendMessage (methodName, callParameter, SendMessageOptions.DontRequireReceiver);
+		}
+	}
 
+	public static HashSet<GameObject> FindGameObjectsWithComponent (Type type)
+	{
+		HashSet<GameObject> objectsWithComponent = new HashSet<GameObject> ();
+		
+		Component[] targetComponents = (Component[])GameObject.FindObjectsOfType (type);
+		for (int index = 0; index < targetComponents.Length; index++) {
+			objectsWithComponent.Add (targetComponents [index].gameObject);
+		}
+		
+		return objectsWithComponent;
 	}
 }
