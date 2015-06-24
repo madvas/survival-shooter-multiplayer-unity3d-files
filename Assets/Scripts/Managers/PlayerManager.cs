c72184bca5c8193bf24540be0c8479aa2baaa0ab
madvas <matus.lestan@gmail.com>
2015-06-24 19:34:19 +0200

    "[GIT AUTO COMMIT]: Wed Jun 24 2015 19:34:19 GMT+0200 (CEST)"

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -5,17 +5,11 @@ public class PlayerManager : MonoBehaviour
 {
 	public float respawnDelay = 2f;
 
-	public static void SpawnPlayer ()
+	public static void InstantiatePlayer ()
 	{
 		PositionData randomPosition = PositionHelper.GetRandomSpawnPosition ();
 		Debug.Log ("spawning player");
 		GameObject player = PhotonNetwork.Instantiate ("Player", randomPosition.position, randomPosition.rotation, 0);
 		GameObjectHelper.SendMessageToAll ("OnPlayerInstantiated", player);
-	}
-
-	public static void DestroyPlayer (GameObject player)
-	{
-		PhotonNetwork.Destroy (player);
-		SpawnPlayer ();
 	}
 }
