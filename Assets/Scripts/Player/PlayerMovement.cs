8f91f3056cc33bf46f29789d494e21fc13efb61a
madvas <matus.lestan@gmail.com>
2015-06-24 21:19:19 +0200

    "[GIT AUTO COMMIT]: Wed Jun 24 2015 21:19:19 GMT+0200 (CEST)"

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -57,7 +57,6 @@ public class PlayerMovement : MonoBehaviour
 
 	void Turning ()
 	{
-		return;
 #if !MOBILE_INPUT
 		// Create a ray from the mouse cursor on screen in the direction of the camera.
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
