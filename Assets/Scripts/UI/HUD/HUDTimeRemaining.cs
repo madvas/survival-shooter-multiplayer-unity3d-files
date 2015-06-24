1466fc616db0e1c58f8704d7b7f70528fa6b99d0
madvas <matus.lestan@gmail.com>
2015-06-24 08:48:35 +0200

    "[GIT AUTO COMMIT]: Wed Jun 24 2015 08:48:35 GMT+0200 (CEST)"

---
+++ Assets/Scripts/UI/HUD/HUDTimeRemaining.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDTimeRemaining : MonoBehaviour
+{
+
+	Text timeText;
+	
+	void Awake ()
+	{
+		timeText = GetComponent<Text> ();
+		RoomTimeManager.onSecondElapsed += OnSecondElapsed;
+	}
+	
+	void OnSecondElapsed (string remainingTime)
+	{
+		timeText.text = remainingTime;
+	}
+}
