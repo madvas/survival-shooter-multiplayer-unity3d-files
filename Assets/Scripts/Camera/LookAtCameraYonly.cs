a3d3c56eb39614e5df534cd5616a0bd965cc797c
madvas <matus.lestan@gmail.com>
2015-06-25 15:53:23 +0200

    "[GIT AUTO COMMIT]: Thu Jun 25 2015 15:53:23 GMT+0200 (CEST)"

---
+++ Assets/Scripts/Camera/LookAtCameraYonly.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCameraYonly : MonoBehaviour
+{
+	public Camera cameraToLookAt;
+	
+	void Update ()
+	{
+		Vector3 v = cameraToLookAt.transform.position - transform.position;
+		v.x = v.z = 0.0f;
+		transform.LookAt (cameraToLookAt.transform.position - v); 
+	}
+}
