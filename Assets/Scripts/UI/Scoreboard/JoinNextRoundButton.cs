77a8a191aecc7ccb34ec1f4af9f487179f8be1c3
madvas <matus.lestan@gmail.com>
2015-06-25 11:48:02 +0200

    "[GIT AUTO COMMIT]: Thu Jun 25 2015 11:48:02 GMT+0200 (CEST)"

---
+++ Assets/Scripts/UI/Scoreboard/JoinNextRoundButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoinNextRoundButton : MonoBehaviour {
+
+	void OnRoundStarted() {
+		gameObject.SetActive(false);
+	}
+
+	void OnPauseStarted() {
+		gameObject.SetActive(true);
+	}
+
+	void public OnClick() {
+
+	}
+}
