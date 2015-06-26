3325b6340b3a5bba147f3bd9f8d8695f1ca821b0
madvas <matus.lestan@gmail.com>
2015-06-26 10:31:24 +0200

    "[GIT AUTO COMMIT]: Fri Jun 26 2015 10:31:24 GMT+0200 (CEST)"

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -17,14 +17,15 @@ public class PlayerManager : MonoBehaviour
 		GameObject player = PhotonNetwork.Instantiate ("Player", Vector3.zero, Quaternion.identity, 0);
 		SkinnedMeshRenderer body = player.FindComponentInChildWithTag<SkinnedMeshRenderer> ("PlayerBodyMesh");
 
-		List<int> availableMaterials = playerMaterials.ToList ().Except (PhotonNetwork.playerList.GetMaterials ());
+		List<int> availableMaterials = Enumerable.Range (0, playerMaterials.Length).Except (PhotonNetwork.playerList.GetMaterials ());
 
 		Debug.Log (PhotonNetwork.playerList.GetMaterials ().Count);
 		Debug.Log (availableMaterials.Count);
 		int materialIndex = Random.Range (0, availableMaterials.Count);
 
+		Debug.Log (materialIndex);
 		PhotonNetwork.player.SetMaterialIndex (materialIndex);
-		body.material = availableMaterials [materialIndex];
+		body.material = playerMaterials [materialIndex];
 
 		player.GetComponent<AudioListener> ().enabled = true;
 		GameObjectHelper.SendMessageToAll ("OnMinePlayerInstantiate", player);
