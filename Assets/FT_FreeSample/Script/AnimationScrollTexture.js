ec3c5af5b60fdf380ad17f231c963b3635beae7e
madvas <matus.lestan@gmail.com>
2015-06-29 22:13:00 +0200

    "[GIT AUTO COMMIT]: Mon Jun 29 2015 22:13:00 GMT+0200 (CEST)"

--- Assets/FT_FreeSample/Script/AnimationScrollTexture.js
+++
@@ -1,8 +0,0 @@
-#pragma strict
-
-var Speed = 0.25;
-function FixedUpdate()
-{
-var offset = Time.time * (-Speed);
-GetComponent.<Renderer>().material.mainTextureOffset = Vector2 (0,offset);
-}
--- Assets/FT_FreeSample/Script/DeadTime.js
+++
@@ -1,5 +0,0 @@
-var deadTime : float;
-
-function Awake () {
-	Destroy (gameObject, deadTime);
-}
