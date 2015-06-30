814f90b0867bc777dbf6638d8641d8deb23f80f2
madvas <matus.lestan@gmail.com>
2015-06-30 10:16:56 +0200

    "[GIT AUTO COMMIT]: Tue Jun 30 2015 10:16:56 GMT+0200 (CEST)"

--- Assets/Scripts/UI/HUD/RoomMessages.cs
+++ Assets/Scripts/UI/HUD/RoomMessages.cs
@@ -5,15 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class RoomMessages : Photon.MonoBehaviour
-{
-	public Color playerChatColor0;
-	public Color playerChatColor1;
-	public Color playerChatColor2;
-	public Color playerChatColor3;
-	public Color playerChatColor4;
-	public Color playerChatColor5;
-	public Color playerChatColor6;
-
+{	
 	InputField messageInput;
 	bool isWriting = false;
 	ScrollRect messagesScrollRect;
@@ -25,21 +17,15 @@ public class RoomMessages : Photon.MonoBehaviour
 		messageInput = GetComponentInChildren<InputField> ();
 		messagesScrollRect = GetComponentInChildren<ScrollRect> ();
 		messagesText = messagesScrollRect.content.gameObject.GetComponent<Text> ();
-		playerChatColors = new Color[7];
-		playerChatColors [0] = playerChatColor0;
-		playerChatColors [1] = playerChatColor1;
-		playerChatColors [2] = playerChatColor2;
-		playerChatColors [3] = playerChatColor3;
-		playerChatColors [4] = playerChatColor4;
-		playerChatColors [5] = playerChatColor5;
-		playerChatColors [6] = playerChatColor6;
 	}
 
 	void Update ()
 	{
 		if (Input.GetKeyDown (KeyCode.Return)) {
 			if (isWriting) {
-				photonView.RPC ("Chat", PhotonTargets.All, messageInput.text);
+				if (messageInput.text.Length > 0) {
+					photonView.RPC ("Chat", PhotonTargets.All, messageInput.text);
+				}
 				messageInput.text = "";
 				isWriting = false;
 				GameObjectHelper.SendMessageToAll ("OnWritingMesssageEnded");
