8d8ba075d9f1d9ebd080b427af5697088c297a1c
madvas <matus.lestan@gmail.com>
2015-06-30 21:50:02 +0200

    "[GIT AUTO COMMIT]: Tue Jun 30 2015 21:50:02 GMT+0200 (CEST)"

--- Assets/Scripts/Player/PlayerSpawning.cs
+++ Assets/Scripts/Player/PlayerSpawning.cs
@@ -26,15 +26,15 @@ public class PlayerSpawning : Photon.MonoBehaviour
 		if (photonView.isMine && isSinking) {
 			transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
 		}
-		if (transform.position.y < -1) {
+		if (isVisible && transform.position.y < -1) {
 			Debug.Log ("setting vis to false");
 			isSinking = false;
 			SetPlayerVisibility (false);
 		}
-		if (!isVisible && transform.position.y == 0f) {
-			Debug.Log ("Cant believe I got here");
-			SetPlayerVisibility (true);
-		}
+//		if (!isVisible && transform.position.y == 0f) {
+//			Debug.Log ("Cant believe I got here");
+//			SetPlayerVisibility (true);
+//		}
 	}
 
 	void OnPhotonInstantiate (PhotonMessageInfo	info)
