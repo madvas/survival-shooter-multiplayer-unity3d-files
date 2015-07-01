f8cba5fca17eec65cf2305a01c36583935671cca
madvas <matus.lestan@gmail.com>
2015-07-01 13:41:05 +0200

    "[GIT AUTO COMMIT]: Wed Jul 01 2015 13:41:05 GMT+0200 (CEST)"

--- Assets/Scripts/UI/HUD/RoomMessages.cs
+++ Assets/Scripts/UI/HUD/RoomMessages.cs
@@ -23,21 +23,23 @@ public class RoomMessages : Photon.MonoBehaviour
 	void Update ()
 	{
 		Event e = Event.current;
-		if (e.type == EventType.keyDown && e.keyCode == KeyCode.Return) {
-			if (isWriting) {
-				if (messageInput.text.Length > 0) {
-					photonView.RPC ("Chat", PhotonTargets.All, messageInput.text);
-				}
-				messageInput.text = "";
-				isWriting = false;
-				messageInput.DeactivateInputField ();
-				GameObjectHelper.SendMessageToAll ("OnWritingMesssageEnded");
-			} else {
-				messageInput.ActivateInputField ();
-				isWriting = true;
-				GameObjectHelper.SendMessageToAll ("OnWritingMesssageStarted");
-			}
-		}
+		Debug.Log (e);
+
+//		if (e. && e.Equals (Event.KeyboardEvent ("[enter]"))) {
+//			if (isWriting) {
+//				if (messageInput.text.Length > 0) {
+//					photonView.RPC ("Chat", PhotonTargets.All, messageInput.text);
+//				}
+//				messageInput.text = "";
+//				isWriting = false;
+//				messageInput.DeactivateInputField ();
+//				GameObjectHelper.SendMessageToAll ("OnWritingMesssageEnded");
+//			} else {
+//				messageInput.ActivateInputField ();
+//				isWriting = true;
+//				GameObjectHelper.SendMessageToAll ("OnWritingMesssageStarted");
+//			}
+//		}
 	}
 
 	void AddMessage (string message)
