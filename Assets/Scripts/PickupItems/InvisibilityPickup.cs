6c87429246dd6a906202dddbd18bf61ef901692f
madvas <matus.lestan@gmail.com>
2015-07-03 23:09:03 +0200

    "[GIT AUTO COMMIT]: Fri Jul 03 2015 23:09:03 GMT+0200 (CEST)"

---
+++ Assets/Scripts/PickupItems/InvisibilityPickup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisibilityPickup : PickupItemBase
+{
+	public int bonusDuration = 20;
+	
+	void OnActivateItemEffect (GameObject player)
+	{
+		player.BroadcastMessageMultiArg ("OnPlayerGoInvisible", bonusDuration);
+	}
+}
