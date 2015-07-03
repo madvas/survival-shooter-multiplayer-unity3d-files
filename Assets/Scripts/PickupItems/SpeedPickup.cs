7b7659aeeaaf53f92afe7ecd6198544ac3ac9eec
madvas <matus.lestan@gmail.com>
2015-07-03 22:34:03 +0200

    "[GIT AUTO COMMIT]: Fri Jul 03 2015 22:34:03 GMT+0200 (CEST)"

---
+++ Assets/Scripts/PickupItems/SpeedPickup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedPickup : PickupItemBase
+{
+	public int increasedSpeed = 15;
+	public int bonusDuration = 20;
+	
+	void OnActivateItemEffect (GameObject player)
+	{
+		player.BroadcastMessageMultiArg ("OnPlayerSpeedChange", increasedSpeed, bonusDuration);
+	}
+}
+
