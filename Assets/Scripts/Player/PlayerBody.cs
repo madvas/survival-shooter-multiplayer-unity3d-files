235c409075bca2c41fbe807109acf1bb44d56947
madvas <matus.lestan@gmail.com>
2015-07-03 23:23:51 +0200

    init commit

--- Assets/Scripts/Player/PlayerBody.cs
+++ Assets/Scripts/Player/PlayerBody.cs
@@ -3,27 +3,28 @@ using System.Collections;
 
 public class PlayerBody : Photon.MonoBehaviour
 {
-
-	void Awake ()
-	{
-
-	}
-
-	void OnPlayerDamageChange (object[] changeData)
-	{
-		if (!photonView.isMine) {
-			return;
-		}
-		
-		photonView.owner.SetInvisibility (true);
-		CancelInvoke ("ResetVisibility");
-		Invoke ("ResetVisibility", bonusDuration);
-	}
-	
-	void ResetVisibility ()
-	{
-		Debug.Log ("damage reset to " + originalDamagePerShot);
-		damagePerShot = originalDamagePerShot;
-		photonView.owner.SetIncreasedDamage (false);
-	}
+//	PlayerSpawning
+//
+//	void Awake ()
+//	{
+//		playerRenderers = GetComponentsInChildren<SkinnedMeshRenderer> ();
+//	}
+//
+//	void OnPlayerDamageChange (object[] changeData)
+//	{
+//		if (!photonView.isMine) {
+//			return;
+//		}
+//		
+//		photonView.owner.SetInvisibility (true);
+//		CancelInvoke ("ResetVisibility");
+//		Invoke ("ResetVisibility", bonusDuration);
+//	}
+//	
+//	void ResetVisibility ()
+//	{
+//		Debug.Log ("damage reset to " + originalDamagePerShot);
+//		damagePerShot = originalDamagePerShot;
+//		photonView.owner.SetIncreasedDamage (false);
+//	}
 }
