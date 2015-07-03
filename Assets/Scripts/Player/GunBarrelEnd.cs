3bfac755e936c42f670c4867f387768f63cac0e6
madvas <matus.lestan@gmail.com>
2015-07-03 20:19:03 +0200

    "[GIT AUTO COMMIT]: Fri Jul 03 2015 20:19:03 GMT+0200 (CEST)"

--- Assets/Scripts/Player/GunBarrelEnd.cs
+++ Assets/Scripts/Player/GunBarrelEnd.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GunBarrelEnd : MonoBehaviour {
+public class GunBarrelEnd : MonoBehaviour
+{
+	float timer;           
 
-	// Use this for initialization
-	void Start () {
-	
+	ParticleSystem gunParticles;                    
+	LineRenderer gunLine;                           
+	AudioSource gunAudio;                           
+	Light gunLight;                                 
+	float effectsDisplayTime = 0.2f;                
+
+
+	void Awake ()
+	{
+		gunParticles = GetComponentInChildren<ParticleSystem> ();
+		gunLine = GetComponentInChildren<LineRenderer> ();
+		gunAudio = gunBarrelEnd.GetComponent<AudioSource> ();
+		gunLight = GetComponentInChildren<Light> ();
+		gunEndTransform = gunBarrelEnd.transform;
+		playerHealth = GetComponent<PlayerHealth> ();
+		anim = GetComponent<Animator> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		timer += Time.deltaTime;
+			
+		if (timer >= timeBetweenBullets * effectsDisplayTime) {
+			DisableEffects ();
+		}
 	}
+
+	public void DrawShot (Vector3 fromPosition, Vector3 hitPositon)
+	{
+		timer = 0f;
+		gunAudio.Play ();
+		gunLight.enabled = true;
+		gunParticles.Stop ();
+		gunParticles.Play ();
+		gunLine.enabled = true;
+		gunLine.SetPosition (0, fromPosition);
+		gunLine.SetPosition (1, hitPositon);
+	}
+
 }
