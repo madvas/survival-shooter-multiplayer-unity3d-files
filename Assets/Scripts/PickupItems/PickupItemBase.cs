05457169fcd2e701916c25197c8dc0a51632dcc3
madvas <matus.lestan@gmail.com>
2015-07-03 10:20:21 +0200

    "[GIT AUTO COMMIT]: Fri Jul 03 2015 10:20:21 GMT+0200 (CEST)"

--- Assets/Scripts/PickupItems/PickupItemBase.cs
+++ Assets/Scripts/PickupItems/PickupItemBase.cs
@@ -14,7 +14,6 @@ public class PickupItemBase : Photon.MonoBehaviour
 			Debug.Log ("OnTriggerEnter() calls Pickup().");
 			Pickup ();
 		}
-		GameObjectHelper.SendMessageToAll ("OnItemPicked", gameObject);
 	}
 
 
@@ -26,6 +25,7 @@ public class PickupItemBase : Photon.MonoBehaviour
 		
 		SentPickup = true;
 		photonView.RPC ("PunPickup", PhotonTargets.AllViaServer);
+		GameObjectHelper.SendMessageToAll ("OnItemPicked", gameObject);
 	}
 
 
