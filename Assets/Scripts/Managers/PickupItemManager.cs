d0785f6d203739ada4ab41c6225619c5bac8537d
madvas <matus.lestan@gmail.com>
2015-07-03 08:30:21 +0200

    "[GIT AUTO COMMIT]: Fri Jul 03 2015 08:30:21 GMT+0200 (CEST)"

--- Assets/Scripts/Managers/PickupItemManager.cs
+++ Assets/Scripts/Managers/PickupItemManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PickupItemsManager : MonoBehaviour
+public class PickupItemManager : MonoBehaviour
 {
 
 	public string prefabName;
--- Assets/Scripts/PickupItems/PickupItem.cs
+++ Assets/Scripts/PickupItems/PickupItem.cs
@@ -1,8 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PickupItem : MonoBehaviour
-{
-
-
-} 
+﻿
