6fa2c6147818028324ccb96816d58a6417b98214
madvas <matus.lestan@gmail.com>
2015-07-03 23:14:03 +0200

    "[GIT AUTO COMMIT]: Fri Jul 03 2015 23:14:03 GMT+0200 (CEST)"

---
+++ Assets/Scripts/Player/PlayerBody.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBody : Photon.MonoBehaviour
+{
+
+	void Awake ()
+	{
+
+	}
+
+	void OnPlayerDamageChange (object[] changeData)
+	{
+		if (!photonView.isMine) {
+			return;
+		}
+		
+		photonView.owner.SetInvisibility (true);
+		CancelInvoke ("ResetVisibility");
+		Invoke ("ResetVisibility", bonusDuration);
+	}
+	
+	void ResetVisibility ()
+	{
+		Debug.Log ("damage reset to " + originalDamagePerShot);
+		damagePerShot = originalDamagePerShot;
+		photonView.owner.SetIncreasedDamage (false);
+	}
+}
