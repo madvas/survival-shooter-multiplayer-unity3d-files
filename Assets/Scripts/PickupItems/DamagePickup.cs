1c2f6119b1f70967549b0f059ea35a238d014591
madvas <matus.lestan@gmail.com>
2015-07-06 09:56:39 +0200

    "[GIT AUTO COMMIT]: Mon Jul 06 2015 09:56:39 GMT+0200 (CEST)"

--- Assets/Scripts/PickupItems/DamagePickup.cs
+++ Assets/Scripts/PickupItems/DamagePickup.cs
@@ -8,6 +8,8 @@ public class DamagePickup : PickupItemBase
 	
 	void OnActivateItemEffect (GameObject player)
 	{
+		Debug.Log ("OnPlayerDamageChange");
+		Debug.Log (player.GetComponent<PhotonView> ().owner.name);
 		player.BroadcastMessageMultiArg ("OnPlayerDamageChange", increasedDamage, bonusDuration);
 	}
 }
